a4b75a96d8883fe7aa4ce512763e059a7398975a
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-05-16 11:46:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from multiprocessing.sharedctypes import Value
+from operator import mod
 from optparse import Values
 
+#Posições atuais da memoria
 posicoes_memoria_acessar = [33,3,11,5]
 dic = {}
 
@@ -18,17 +20,34 @@ def imprimir_cache(cache):
     for chave in cache.keys():
         print("{} {}".format(chave,cache[chave]))
     
-def mapeamento_direto(tamanho_cache, pos_memoria):
+def mapeamento_direto(tamanho_cache, pos_memoria, dic):
+    miss = 0
+    hit = 0
+    inicializar_cache(tamanho_cache, dic)
     
-    #Posições atuais da memoria
-    
-    
+    #Acessando cada elemento da lista e usando-o na conta
+    for i in range(len(pos_memoria)):
+        posicao_cache = pos_memoria[i] % tamanho_cache
+        #Acessando cada chave do dicionario e comparando se ele já existe,
+        #caso exista, ele vai mudar a posição do valor dessa chave para o
+        #da lista
+        for chave in dic.keys():
+            if chave == posicao_cache:
+                miss += 1
+                dic[chave] = pos_memoria[i]
+                
+    print("Deram {} miss".format(miss))            
+    imprimir_cache(dic)
+                
+        
            
        
        
         
         
 tamanho_cache = int(input("Informe o tamanho da cache"))
-inicializar_cache(tamanho_cache, dic)
-imprimir_cache(dic)
+#inicializar_cache(tamanho_cache, dic)
+#imprimir_cache(dic)
+
+mapeamento_direto(tamanho_cache,posicoes_memoria_acessar,dic)
 
